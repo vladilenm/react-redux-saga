570332477ffaea60f8f56942d29fed339185a48d
Veys Aliyev <40066034+MDReal32@users.noreply.github.com>
2020-11-21 16:45:49 +0400

    Update sagas.js

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -19,6 +19,6 @@ function* sagaWorker() {
 }
 
 async function fetchPosts() {
-  const response = await fetch('ttps://jsonplaceholder.typicode.com/posts?_limit=5')
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
   return await response.json()
 }
