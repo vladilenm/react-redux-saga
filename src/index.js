671c0e06f229b501076cb9429146fd20c105021a
Vitali <kantyshvitali@gmail.com>
2021-01-03 17:38:56 +0300

    fix: redux devtools extentions

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ const store = createStore(rootReducer, compose(
   applyMiddleware(
     thunk, forbiddenWordsMiddleware, saga
   ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
 ))
 
 saga.run(sagaWatcher)
